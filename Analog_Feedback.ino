8d2a8bf0f80f9301b76c484f912f996d7df8d507
Raffique <Raffique@users.noreply.github.com>
2017-02-27 07:49:44 -0500

    Update Analog_Feedback.ino

--- Analog_Feedback.ino
+++ Analog_Feedback.ino
@@ -1,30 +1,32 @@
 #include <Servo.h>
-Servo servo1;
+Servo myservo;
 
 void setup() {
   Serial.begin(9600);
   }
 
 void loop() {
+  //section that records the position of the servo
   Serial.println("Recording"); 
   delay(5000);
-  int num [200];
-  for (int n=1; n< 20; n++)
+  int num [200]; //aray that stores positions
+  for (int n=1; n< 20; n++) // n in the for loop controls the number of postions stored
     {
-    int pos = analogRead(A0);
-    num[n]= (pos - 111)/1.9723;
+    int pos = analogRead(A0);  //reads positions 
+    num[n]= (pos - 111)/1.9723; // coonvertsthe feedback into values between 0-180
     Serial.print(n);
-    delay(500);
+    delay(500); // increase or decresing the delay can make the rotation of the servo smoother
     }
 
+ //section that plays back the positions 
  Serial.println("Playback");
   delay(5000);
-  servo1.attach(8);
-  for (int m=1; m<20; m++)
+  myservo.attach(8); //attaches servo to pin 8
+  for (int m=1; m<20; m++) // m controls number of positions played
     {
-      servo1.write(num[m]);
-      delay(500);
+      myservo.write(num[m]);
+      delay(500); //delay controls rotation speed
     }
-  servo1.detach();
+  myservo.detach(); //dettach servo, this is important so that the servo can record aagin after. you cant move the servo while its attach it will resist movement
     
 }
